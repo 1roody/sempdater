7a6674ca572602be7e407ce3ccdeaec3846fc497
1roody <devjoaorodrigo@gmail.com>
2024-11-03 12:18:23 -0300

    new check branches functions

--- src/handle/handle-branches.go
+++ src/handle/handle-branches.go
@@ -7,24 +7,25 @@ import (
 )
 
 func checkoutBranch() string {
-	if branchExists("develop") {
-		return switchBranch("develop")
-	} else if branchExists("homolog") {
-		return switchBranch("homolog")
-	} else {
-		fmt.Printf("%s> Neither 'develop' or 'homolog' branches exist. Using master/main instead...%s\n", assets.Red, assets.Nc)
-		return RunCommand("git", "rev-parse", "--abbrev-ref", "HEAD")
+	if tryCheckout("develop") {
+		return "develop"
 	}
+	if tryCheckout("homolog") {
+		return "homolog"
+	}
+
+	fmt.Printf("%s> Neither 'develop' nor 'homolog' branches exist. Staying on current branch.%s\n", assets.Red, assets.Nc)
+	currentBranch := RunCommand("git", "rev-parse", "--abbrev-ref", "HEAD")
+	return currentBranch
 }
 
-func branchExists(branch string) bool {
-	cmd := exec.Command("git", "show-ref", "--quiet", "refs/heads/"+branch)
-	return cmd.Run() == nil
-}
-
-func switchBranch(branch string) string {
-	fmt.Printf("%s> Branch '%s' exists. Attempting to checkout...%s\n", assets.Cyan, branch, assets.Nc)
-	RunCommand("git", "checkout", branch)
-	fmt.Printf("%s> Switched to %s%s\n", assets.Green, branch, assets.Nc)
-	return branch
+func tryCheckout(branch string) bool {
+	fmt.Printf("%s> Attempting to checkout branch '%s'...%s\n", assets.Cyan, branch, assets.Nc)
+	cmd := exec.Command("git", "checkout", branch)
+	if err := cmd.Run(); err == nil {
+		fmt.Printf("%s> Successfully switched to branch '%s'.%s\n", assets.Green, branch, assets.Nc)
+		return true
+	}
+	fmt.Printf("%s> Branch '%s' does not exist or checkout failed.%s\n", assets.Red, branch, assets.Nc)
+	return false
 }
